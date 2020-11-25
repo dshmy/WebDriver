fb8684353057c8e7a1a77c0cecc0efeab72c9e86
dshmy <d.shmyrov12@gmail.com>
2020-11-25 23:04:18 +0300

    fixed

--- src/test/java/FiveElementTest.java
+++ src/test/java/FiveElementTest.java
@@ -7,41 +7,37 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.Keys;
 import java.util.List;
-import org.openqa.selenium.opera.OperaOptions;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class FiveElementTest {
     WebDriver driver;
 
     @BeforeMethod(alwaysRun = true)
     public void driverInitiate(){
-        OperaOptions option = new OperaOptions();
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("--disable-notifications");
+        option.addArguments("--disable-popup-blocking");
         option.addArguments("--window-size=1600,900");
-        driver = new OperaDriver(option);
+        driver = new ChromeDriver(option);
     }
 
     @Test
     public void addPhoneToBasketTest() throws InterruptedException {
         driver.get("https://5element.by");
-        WebElement openSearchString = driver.findElement(By.xpath("/html/body/header/div/div[2]/div[3]/div[1]/form/div/input[2]"));
+        WebElement openSearchString = driver.findElement(By.xpath("//div[@class=\"searchbox js-search-main\"]"));
         openSearchString.click();
         WebElement inputString = driver.findElement(By.xpath("//*[@id='q']"));
-        inputString.sendKeys("Huawei");
-        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("/html/body/div[10]/div/div[1]/div/div[2]/div/div[2]/div/div[1]/div[1]/a")));
-        WebElement pickPhone = driver.findElement(By.xpath("/html/body/div[10]/div/div[1]/div/div[2]/div/div[2]/div/div[1]/div[1]/a"));
+        inputString.sendKeys("Смартфон Huawei Y9s (STK-L21) светло-голубой");
+        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//a[@href=\"/products/652963-smartfon-huawei-y9s-stk-l21-svetlo-goluboy?q=смартфон huawei y9s (stk-l21) светло-голубой\"]")));
+        WebElement pickPhone = driver.findElement(By.xpath("//a[@href=\"/products/652963-smartfon-huawei-y9s-stk-l21-svetlo-goluboy?q=смартфон huawei y9s (stk-l21) светло-голубой\"]"));
         pickPhone.click();
-        Thread.sleep(40000);
-        WebElement closeBanner = driver.findElement(By.xpath("/html/body/div[1]/div[1]"));
-        closeBanner.click();
         new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//button[@class=\"spec-product-right-button button js-to-cart\"]")));
         WebElement addToBasketButton = driver.findElement(By.xpath("//button[@class=\"spec-product-right-button button js-to-cart\"]"));
         addToBasketButton.click();
         driver.get("https://5element.by/cart");
-        Thread.sleep(40000);
-        WebElement closeBannerInBasket = driver.findElement(By.xpath("/html/body/div[1]/div[1]"));
-        closeBannerInBasket.click();
         List<WebElement> basketItems = driver.findElements(By.xpath("//div[@class=\"cart-products__row cart-products__row_ga\"]"));
         Assert.assertTrue(basketItems.size()>0);
 
